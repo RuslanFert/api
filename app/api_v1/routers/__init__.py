d48ea4735c7507f2c202cb41dd9b63e0d9f2a184
Rinat <rinat209@mail.ru>
2023-08-22 17:28:49 +0300

    add_fastapi_app

---
+++ app/api_v1/routers/__init__.py
@@ -0,0 +1,3 @@
+from app.api_v1.routers.admin_router import admin_router
+
+__all__ = [admin_router]
---
+++ app/const.py
@@ -0,0 +1,4 @@
+FORBIDDEN_ERROR = {
+    "code": "forbidden",
+    "description": "You hasn`t permissions, ask admin for access.",
+}
